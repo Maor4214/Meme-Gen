f9d9c13171ef253794dfc93f2463d8ecc59bce0d
Maor <Maoryad@gmail.com>
2024-12-19 21:14:46 +0200

    Add a render img function

--- js/Controllers/gallery.controller.js
+++ js/Controllers/gallery.controller.js
@@ -1,1 +1,16 @@
 'use strict'
+
+function onInit() {
+  renderGallery()
+  //remmber to add a hidden class to the other sections!!
+}
+
+function renderGallery() {
+  const imgs = gImgs
+  const elGallery = document.querySelector('.main-gallery')
+  const strHtmls = imgs.map(
+    (img) => `
+        <img src="${img.imgUrl}" alt="${img.keywords[0]}">`
+  )
+  elGallery.innerHTML = strHtmls.join('')
+}
--- js/Services/gallery.service.js
+++ js/Services/gallery.service.js
@@ -7,15 +7,15 @@ _createImgs()
 function _createImgs() {
   gImgs = [
     _createImg('imgs/1.jpg', ['funny', 'person']),
-    _createImg('imgs/2.jpg', ['cute, pets']),
+    _createImg('imgs/2.jpg', ['cute', 'pets']),
   ]
 }
 
-function _createImg(img, keywords) {
+function _createImg(imgUrl, keywords) {
   gIdCount++
   return {
     id: gIdCount,
-    img,
+    imgUrl,
     keywords,
   }
 }
